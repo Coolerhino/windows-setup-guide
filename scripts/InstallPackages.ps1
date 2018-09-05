0e05b0d9e89d5693939b8807b2708cfe87028228
Emilian Lato <emilian3310@gmail.com>
2018-09-06 00:41:19 +0200

    add scripts folder

--- scripts/InstallPackages.ps1
+++ scripts/InstallPackages.ps1
@@ -3,6 +3,7 @@
 
 cinst googlechrome
 #cinst goodle-backup-and-sync #manual is better?
+cinst opera --params "/NoDesktopShortcut"
 cinst spotify
 cinst vscode #osobny skrypt chyba do rozszerzen do niego
 cinst git.install --params "/GitOnlyOnPath /NoShellIntegration"
@@ -21,11 +22,15 @@ cinst python
 cinst androidstudio
 cinst wincdemu
 cinst telegram.install
+cinst miktex.install
+cinst texstudio
 cinst utorrent
 cinst shotcut
 cinst rainmeter
 
-# napi projekt manualnie
+#napi projekt manualnie
+#vs ultimate 17 manualnie
+#linqpad 5 na razie manualnie
 
 #choco install - cinst
 #choco upgrade - cup
