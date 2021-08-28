076fe75d9635e7a014dd75cd87152b3bc00a240a
Emilian Lato <emilian3310@gmail.com>
2021-08-28 23:34:28 +0200

    Update InstallPackages.ps1

--- scripts/InstallPackages.ps1
+++ scripts/InstallPackages.ps1
@@ -1,5 +1,6 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force;
-#Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+#[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+#iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 cinst googlechrome
 #cinst goodle-backup-and-sync #manual is better?
